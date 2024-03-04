e29b6bd22bd808c816790026263b3dc14de80ea3
Michael Parsakia <28745698+mparsakia@users.noreply.github.com>
2024-03-04 12:18:47 -0800

    Revert "lets see what gpt does"
    This reverts commit 796b6e161412debb210243e588df32fe86518ddf.

--- site/src/side-panel/side-panel.js
+++ site/src/side-panel/side-panel.js
@@ -14,35 +14,6 @@ export class SidePanel extends BaseElement {
     this.render();
     this.sidePanels = this.querySelector(".side-panel__panels");
     this.subscribe("members-updated", this.handleUpdatedMembers.bind(this));
-
-    // Drag and Drop functionality
-    let draggedItem = null;
-
-    this.sidePanels.addEventListener('dragstart', (e) => {
-        draggedItem = e.target;
-        setTimeout(() => e.target.style.opacity = "0.5", 0);
-    });
-
-    this.sidePanels.addEventListener('dragend', (e) => {
-        setTimeout(() => {
-            draggedItem.style.opacity = "";
-            draggedItem = null;
-        }, 0);
-    });
-
-    this.sidePanels.addEventListener('dragover', (e) => {
-        e.preventDefault();
-    });
-
-    this.sidePanels.addEventListener('drop', (e) => {
-        e.preventDefault();
-        if (e.target.classList.contains("player-panel")) {
-            this.sidePanels.insertBefore(draggedItem, e.target.nextSibling);
-            // Update order in localStorage
-            const order = Array.from(this.sidePanels.children).map(panel => panel.id);
-            localStorage.setItem('panelOrder', JSON.stringify(order));
-        }
-    });
   }
 
   disconnectedCallback() {
