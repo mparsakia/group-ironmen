d86177749e03cfbf4997ab4d836fb3793eed4c64
Michael Parsakia <28745698+mparsakia@users.noreply.github.com>
2024-03-04 21:57:35 -0800

    Update canvas-map.js

--- site/src/canvas-map/canvas-map.js
+++ site/src/canvas-map/canvas-map.js
@@ -60,7 +60,7 @@ export class CanvasMap extends BaseElement {
         progress: 1,
       }),
       maxZoom: 6,
-      minZoom: 0.1,
+      minZoom: 0.15,
       isDragging: false,
     };
     this.cursor = {
